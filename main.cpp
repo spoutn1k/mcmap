5226ddadbe1bfb92c0d517071acb1cfd6a9323d5
Zahl <noreply@spieleplanet.eu>
2011-09-24 12:35:18 +0200

    added CC-BY-SA license

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 /***
  * mcmap - create isometric maps of your minecraft alpha/beta world
- * v2.1a, 00-2011 by Zahl
+ * v2.1a, 09-2011 by Zahl
  *
- * mcmap is licensed under the Creative Commons Attribution-ShareAlike 3.0
- * Unported License. To view a copy of this license, visit
- * http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
- * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ * Copyright 2011, Zahl. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ * 
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
