spoutn1k/mcmap

commit 6b00a3430d080ecf0b47ed0e13be535b7f2d5eef

-nowater option works with colors.txt file

worldloader.cpp
@@ -1032,6 +1032,7 @@ static inline void assignBlock(const uint8_t &block, uint8_t* &targetBlock, int 
 {
 	if (block == WOOL || block == LOG || block == LEAVES || block == STEP || block == DOUBLESTEP || block == WOOD || block == WOODEN_STEP || block == WOODEN_DOUBLE_STEP 
 		|| block == 95 || block == 160 || block == 159 || block == 171 || block == 38 || block == 175 || block == SAND || block == 153 || block == 166 || block == 167
+		|| block == WATER || block == STAT_WATER
 		|| block == 141 || block == 142 || block == 158 || block == 149 || block == 157
 		|| block == 131 || block == 132 || block == 150 || block == 147 || block == 148 || block == 68 || block == 69 || block == 70
 		|| block == 72 || block == 77 || block == 143 || block == 36) {  //three last lines contains colors for carpets
@@ -1042,7 +1043,8 @@ static inline void assignBlock(const uint8_t &block, uint8_t* &targetBlock, int 
 			col = (justData[(y + (z + (x * CHUNKSIZE_Z)) * CHUNKSIZE_Y) / 2] >> ((y % 2) * 4)) & 0xF;
 		}
 		if (block == 131 || block == 132 || block == 150 || block == 147 || block == 148 || block == 68 || block == 69 || block == 70
-			|| block == 72 || block == 77 || block == 143 || block == 36 || block == 166) {		//not visible blocks replaced to air, therefore we have few ID's more
+			|| block == 72 || block == 77 || block == 143 || block == 36 || block == 166
+			|| (g_NoWater && (block == WATER || block == STAT_WATER))) {		//not visible blocks replaced to air, therefore we have few ID's more
 				*targetBlock++ = 0;
 		} else if (block == 141 || block == 142) {		//carrots and potatoes -> wheat
 				*targetBlock++ = 59;

colors.cpp
@@ -44,13 +44,8 @@ void loadColors()
 	SETCOLORNOISE(WOOD, 157,128,79,255, 11);
 	SETCOLOR(6, 120,120,120,0);
 	SETCOLOR(7, 84,84,84,255);
-	if (g_NoWater) {
-		SETCOLOR(WATER, 255,255,255,0);
-		SETCOLOR(STAT_WATER, 255,255,255,0);
-	} else {
-		SETCOLOR(WATER, 38,92,225,36);
-		SETCOLOR(STAT_WATER, 38,92,225,36);
-	}
+	SETCOLOR(WATER, 38,92,225,36);
+	SETCOLOR(STAT_WATER, 38,92,225,36);
 	SETCOLOR(10, 255,90,0,255);
 	SETCOLOR(11, 255,90,0,255);
 	SETCOLORNOISE(SAND, 220,212,160,255, 14);

commit c01a82bfed08312731c11173e90f38b5be6b7223

Fix assigning Flowing Water (regression from 6b00a3430d080ecf0b47ed0e13be535b7f2d5eef)
when rendering WITH water (-nowater not set) the assignBlock water case did not hit
and ended in the default else tree, which was only supposed to happen for doublesteps.
Flowing water has a "col" value, depending on its height, we did hit the
sandstone & wood & cobblestone doublestep case, which is why only height flowing water
was replaced and the "low" flowing water not.

worldloader.cpp
@@ -1032,7 +1032,7 @@ static inline void assignBlock(const uint8_t &block, uint8_t* &targetBlock, int 
 {
 	if (block == WOOL || block == LOG || block == LEAVES || block == STEP || block == DOUBLESTEP || block == WOOD || block == WOODEN_STEP || block == WOODEN_DOUBLE_STEP 
 		|| block == 95 || block == 160 || block == 159 || block == 171 || block == 38 || block == 175 || block == SAND || block == 153 || block == 166 || block == 167
-		|| block == WATER || block == STAT_WATER
+		|| (g_NoWater && (block == WATER || block == STAT_WATER))
 		|| block == 141 || block == 142 || block == 158 || block == 149 || block == 157
 		|| block == 131 || block == 132 || block == 150 || block == 147 || block == 148 || block == 68 || block == 69 || block == 70
 		|| block == 72 || block == 77 || block == 143 || block == 36) {  //three last lines contains colors for carpets
@@ -1044,7 +1044,7 @@ static inline void assignBlock(const uint8_t &block, uint8_t* &targetBlock, int 
 		}
 		if (block == 131 || block == 132 || block == 150 || block == 147 || block == 148 || block == 68 || block == 69 || block == 70
 			|| block == 72 || block == 77 || block == 143 || block == 36 || block == 166
-			|| (g_NoWater && (block == WATER || block == STAT_WATER))) {		//not visible blocks replaced to air, therefore we have few ID's more
+			|| block == WATER || block == STAT_WATER) {		//not visible blocks replaced to air, therefore we have few ID's more
 				*targetBlock++ = 0;
 		} else if (block == 141 || block == 142) {		//carrots and potatoes -> wheat
 				*targetBlock++ = 59;