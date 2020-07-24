c465f04ae0f316069d48d20cc28c0eb3678351d8
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-07-24 07:03:13 +0100

    Fixed pre-1.16 rendering errors
    Revamped the section access method and added documentation to explain
    what in tarnation is happening in there.
    Adjusted data types for possible values.

--- src/worldloader.cpp
+++ src/worldloader.cpp
@@ -275,47 +275,33 @@ void Terrain::Data::loadChunk(const uint32_t offset, FILE *regionHandle,
   inflateChunk(sections);
 }
 
-int16_t blockAtPost116(const uint64_t length,
+int16_t blockAtPost116(const uint64_t index_length,
                        const std::vector<int64_t> *blockStates, uint8_t x,
                        uint8_t z, uint8_t y) {
-  // The `BlockStates` array contains data on the section's blocks. You have
-  // to extract it by understanfing its structure.
-  //
-  // Although it is a array of long values, one must see it as an array of
-  // block indexes, whose element size depends on the size of the Palette.
-  // This routine locates the necessary long, extracts the block with bit
-  // comparisons, and cross-references it in the palette to get the block
-  // name.
-  //
-  // NEW in 1.16, longs are padded by 0s when a block cannot fit.
+  // NEW in 1.16, longs are padded by 0s when a block cannot fit, so no more
+  // overflow to deal with !
 
-  const uint64_t index = (x & 0x0f) + ((z & 0x0f) + (y & 0x0f) * 16) * 16;
+  const uint32_t index = (x & 0x0f) + ((z & 0x0f) + (y & 0x0f) * 16) * 16;
 
-  // The length of a block index has to be coded on the minimal possible size,
-  // which is the logarithm in base2 of the size of the palette, or 4 if the
-  // logarithm is smaller.
+  // Determine how many indexes each long holds
+  const uint8_t blocksPerLong = 64 / index_length;
 
-  // First, determine how many blocks are in each long. There is an implicit
-  // `floor` here, needed later.
-  const uint8_t blocksPerLong = 64 / length;
-
-  // Next, calculate where in the long array is the long containing the block.
-  const uint64_t longIndex = index / blocksPerLong;
+  // Calculate where in the long array is the long containing the right index.
+  const uint16_t longIndex = index / blocksPerLong;
 
   // Once we located a long, we have to know where in the 64 bits
   // the relevant block is located.
-  const uint64_t padding = (index - longIndex * blocksPerLong) * length;
+  const uint8_t padding = (index - longIndex * blocksPerLong) * index_length;
 
   // Bring the data to the first bits of the long, then extract it by bitwise
   // comparison
-  const uint64_t blockIndex =
-      ((*blockStates)[longIndex] >> padding) & ((1l << length) - 1);
+  const uint16_t blockIndex = ((*blockStates)[longIndex] >> padding) &
+                              ((uint64_t(1) << index_length) - 1);
 
-  // Lower data now contains the index in the palette
   return blockIndex;
 }
 
-int16_t blockAtPre116(const uint64_t length,
+int16_t blockAtPre116(const uint64_t index_length,
                       const std::vector<int64_t> *blockStates, uint8_t x,
                       uint8_t z, uint8_t y) {
   // The `BlockStates` array contains data on the section's blocks. You have to
@@ -324,36 +310,61 @@ int16_t blockAtPre116(const uint64_t length,
   // Although it is a array of long values, one must see it as an array of block
   // indexes, whose element size depends on the size of the Palette. This
   // routine locates the necessary long, extracts the block with bit
-  // comparisons, and cross-references it in the palette to get the block name.
-
-  const uint64_t index = (x & 0x0f) + ((z & 0x0f) + (y & 0x0f) * 16) * 16;
-
+  // comparisons.
+  //
   // The length of a block index has to be coded on the minimal possible size,
   // which is the logarithm in base2 of the size of the palette, or 4 if the
   // logarithm is smaller.
 
+  // The index of the block in the section itself, if every block had a number
+  // according to its order
+  const uint32_t index = (x & 0x0f) + ((z & 0x0f) + (y & 0x0f) * 16) * 16;
+
   // We skip the `position` first blocks, of length `size`, then divide by 64 to
   // get the number of longs to skip from the array
-  const uint64_t skip_longs = index * length >> 6;
+  const uint16_t skip_longs = (index * index_length) >> 6;
 
   // Once we located the data in a long, we have to know where in the 64 bits it
   // is located. This is the remaining of the previous operation
-  const int64_t padding = index * length & 63;
+  const int8_t padding = (index * index_length) & 63;
 
-  // Craft a mask from the length of the block index and the padding, the apply
-  // it to the long
-  const uint64_t mask = ((1l << length) - 1) << padding;
-  uint64_t lower_data = ((*blockStates)[skip_longs] & mask) >> padding;
+  // Sometimes the data of an index does not fit entirely into a long, so we
+  // check if there is overflow
+  const int8_t overflow =
+      (padding + index_length > 64 ? padding + index_length - 64 : 0);
 
-  // Sometimes the length of the index does not fall entirely into a long, so
-  // here we check if there is overflow and extract it too
-  const int64_t overflow = padding + length - 64;
+  // This complicated expression extracts the necessary bits from the current
+  // long.
+  //
+  // Lets say we need the following bits in a long (not to scale):
+  // 10011100111001110011100
+  //    ^^^^^
+  // We do this by shifting (>>) the data by padding, to get the relevant bits
+  // on the end of the long:
+  // ???????????????10011100
+  //                   ^^^^^
+  // We then apply a mask to get only the relevant bits:
+  // ???????????????10011100
+  // 00000000000000000011111 &
+  // 00000000000000000011100 <- result
+  //
+  // The mask is made at the size of the data, using the formula (1 << n) - 1,
+  // the resulting bitset is of the following shape: 0...01...1 with n 1s.
+  //
+  // If there is an overflow, the mask size is reduced, as not to catch noise
+  // from the padding (ie the interrogation points earlier) that appear on
+  // ARM32.
+  uint16_t lower_data = ((*blockStates)[skip_longs] >> padding) &
+                        ((uint64_t(1) << (index_length - overflow)) - 1);
+
   if (overflow > 0) {
-    const uint64_t upper_data =
-        (*blockStates)[skip_longs + 1] & ((1l << overflow) - 1);
-    lower_data = lower_data | upper_data << (length - overflow);
+    // The exact same process is used to catch the overflow from the next long
+    const uint16_t upper_data =
+        ((*blockStates)[skip_longs + 1]) & ((uint64_t(1) << overflow) - 1);
+    // We then associate both values to create the final value
+    lower_data = lower_data | (upper_data << (index_length - overflow));
   }
 
-  // Lower data now contains the index in the palette
+  // lower_data now contains the index in the palette
   return lower_data;
 }
