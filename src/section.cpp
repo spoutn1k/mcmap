f486eeb90d2bec094d8297e2e0e782821aaca289
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-10-03 13:48:23 -0700

    Interpreter now section-wide

--- src/section.cpp
+++ src/section.cpp
@@ -3,7 +3,8 @@
 const Colors::Block _void;
 
 Section::Section(const nbt::NBT &raw_section, const int dataVersion,
-                 const Colors::Palette &defined) {
+                 const Colors::Palette &defined)
+    : Section() {
   if (!raw_section.contains("Palette") ||
       !raw_section.contains("BlockStates")) {
     return;
@@ -20,31 +21,27 @@ Section::Section(const nbt::NBT &raw_section, const int dataVersion,
   sectionInterpreter interpreter;
 
   if (dataVersion < 2534)
-    interpreter = blockAtPre116;
+    interpreter = sectionAtPre116;
   else
-    interpreter = blockAtPost116;
+    interpreter = sectionAtPost116;
 
-  for (int i = 0; i < 4096; i++)
-    blocks[i] = interpreter(blockBitLength, blockStates, i % 16, (i / 16) % 16,
-                            i / 256);
+  interpreter(blockBitLength, blockStates, blocks);
 
   pickColors(defined);
 }
 
 void Section::pickColors(const Colors::Palette &all) {
-  uint16_t colorIndex = 0;
-
   for (auto &color : palette) {
     const string namespacedId = color["Name"].get<string>();
     auto defined = all.find(namespacedId);
 
     if (defined == all.end()) {
       logger::error("Color of block {} not found\n", namespacedId);
-      colors[colorIndex++] = &_void;
+      colors[max_colors++] = &_void;
     } else {
-      colors[colorIndex++] = &defined->second;
+      colors[max_colors++] = &defined->second;
       if (namespacedId == "minecraft:beacon")
-        beaconIndex = colorIndex - 1;
+        beaconIndex = max_colors - 1;
     }
   }
 }
--- src/section.h
+++ src/section.h
@@ -3,12 +3,12 @@
 #include <nbt/nbt.hpp>
 
 struct Section {
-  uint16_t beaconIndex;
+  uint8_t max_colors, beaconIndex;
   uint8_t blocks[4096];
   std::vector<nbt::NBT> palette;
   const Colors::Block *colors[256];
 
-  Section() : beaconIndex(std::numeric_limits<uint16_t>::max()){};
+  Section() : max_colors(0), beaconIndex(std::numeric_limits<uint8_t>::max()){};
   Section(const nbt::NBT &, const int, const Colors::Palette &);
   Section(Section &&other) : palette(std::move(other.palette)) {
     memmove(blocks, other.blocks, 4096);
