c407995c7e8ac38e030a975a1074406bcec35a11
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2020-12-13 13:55:30 -0800

    Further refactor colors; colors tests

--- src/colors.h
+++ src/colors.h
@@ -17,7 +17,6 @@ using std::string;
 #define PGREEN 1
 #define PBLUE 2
 #define PALPHA 3
-#define PNOISE 4
 
 inline void blend(uint8_t *const destination, const uint8_t *const source) {
   if (!source[PALPHA])
@@ -76,12 +75,10 @@ const std::map<string, list<int>> markerColors = {
 };
 
 struct Color {
-  // Red, Green, Blue
-  uint8_t R, G, B;
-  // Transparency, and legacy setting
-  uint8_t ALPHA, NOISE;
+  // Red, Green, Blue, Transparency
+  uint8_t R, G, B, ALPHA;
 
-  Color() { R = G = B = ALPHA = NOISE = 0; }
+  Color() { R = G = B = ALPHA = 0; }
 
   Color(list<int> values) : Color() {
     uint8_t index = 0;
@@ -124,9 +121,9 @@ struct Color {
 };
 
 struct Block {
-  Colors::Color primary, secondary; // 12 bytes
+  Colors::Color primary, secondary; // 8 bytes
   Colors::BlockTypes type;
-  Colors::Color light, dark; // 12 bytes
+  Colors::Color light, dark; // 8 bytes
 
   Block() : primary(), secondary() { type = Colors::BlockTypes::FULL; }
 
@@ -152,30 +149,30 @@ struct Block {
   }
 
   bool operator==(const Block &other) const {
-    return (primary == other.primary);
+    return memcmp(this, &other, 12) == 0;
   }
+
+  bool operator!=(const Block &other) const { return !operator==(other); }
 };
 
 typedef map<string, Colors::Block> Palette;
 
 struct Marker {
   int64_t x, z;
-  string color_name;
   Block color;
 
-  Marker() : color_name("white") {
+  Marker() {
     x = std::numeric_limits<int64_t>::max();
     z = std::numeric_limits<int64_t>::max();
   }
 
-  Marker(int64_t x, int64_t z, string c) : x(x), z(z), color_name(c) {
-    auto marker = markerColors.find(color_name);
-    if (marker == markerColors.end()) {
-      fprintf(stderr, "Invalid marker color: %s\n", color_name.c_str());
-      color_name = "white";
+  Marker(int64_t x, int64_t z, string c) : x(x), z(z) {
+    if (markerColors.find(c) == markerColors.end()) {
+      logger::error("Invalid marker color: {}\n", c);
+      c = "white";
     }
 
-    color = Block(BlockTypes::drawBeam, markerColors.find(color_name)->second);
+    color = Block(BlockTypes::drawBeam, markerColors.find(c)->second);
   };
 };
 
