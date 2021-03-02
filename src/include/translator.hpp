646dde90da531d1d5013a695ea1e9483d598d142
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2021-03-01 18:09:38 -0800

    NBT-independent translator

---
+++ src/include/translator.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+enum DataType { SHORT, INT, LONG, FLOAT, DOUBLE };
+
+void swap16(const uint8_t *src, uint8_t *dest) {
+  dest[0] = src[1];
+  dest[1] = src[0];
+}
+
+void swap32(const uint8_t *src, uint8_t *dest) {
+  dest[0] = src[3];
+  dest[1] = src[2];
+  dest[2] = src[1];
+  dest[3] = src[0];
+}
+
+void swap64(const uint8_t *src, uint8_t *dest) {
+  dest[0] = src[7];
+  dest[1] = src[6];
+  dest[2] = src[5];
+  dest[3] = src[4];
+  dest[4] = src[3];
+  dest[5] = src[2];
+  dest[6] = src[1];
+  dest[7] = src[0];
+}
+
+template <typename ArithmeticType,
+          typename std::enable_if<std::is_arithmetic<ArithmeticType>::value,
+                                  int>::type = 0>
+ArithmeticType translate(const uint8_t *_bytes) {
+  uint8_t buffer[8];
+  std::size_t bytes_n = std::alignment_of<ArithmeticType>();
+
+  switch (bytes_n) {
+  case 2:
+    swap16(_bytes, buffer);
+    break;
+
+  case 4:
+    swap32(_bytes, buffer);
+    break;
+
+  case 8:
+    swap64(_bytes, buffer);
+    break;
+  }
+
+  return *static_cast<ArithmeticType *>(buffer);
+}
