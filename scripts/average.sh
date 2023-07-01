37c25a9831e8996335c1cb42ae5a8a187d0c0ec4
Netherwhal <mail@netherwhal.com>
2023-07-01 11:02:06 +0200

    Add 1.20.1 colors

--- scripts/average.sh
+++ scripts/average.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 # Requires ImageMagick
 # Outputs the color average of the image passed as an argument
 
-echo -ne "`basename "$1"`\t"
-convert "$1" -resize 1x1 txt:- | grep -Po "#[[:xdigit:]]{6}" | tr A-F a-f
+set -eo pipefail
+
+printf '%s\t%s\n' \
+  "$(basename "${1}")" \
+  "$(convert "${1}" -resize 1x1 txt:- | grep -Po "#[[:xdigit:]]{6}" | tr A-F a-f)"
